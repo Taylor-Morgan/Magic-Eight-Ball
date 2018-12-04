8cd0422f86be92165835a03a432f63b33d91790c
Taylor Morgan <morgan.taylor86@gmail.com>
2018-12-03 16:19:24 -0800

    Finish Javascript functionality
    The Magic Eight Ball is now fully functional (for now). Further improvements will focus on aesthetics.

--- main.js
+++ main.js
@@ -2,18 +2,21 @@
 
 const answer0 = 'It is certain.';
 const answer1 = 'It is decidedly so.';
-const answer2 = 'Solar Flares! Try asking again.';
-const answer3 = 'Sources say "No".';
-const answer4 = "Don't count on it.";
-const answer5 = 'My reply is no.';
-const answer6 = 'My reply is no.';
+const answer2 = 'Sources say "No".';
+const answer3 = "Don't count on it.";
+const answer4 = "Can't tell right now";
+const answer5 = 'Solar Flares! Try asking again.';
 
-const responseArray = [answer0, answer1, answer2, answer3, answer4, answer5, answer6];
+const responseArray = [answer0, answer1, answer2, answer3, answer4, answer5];
+
 
 function getResponse() {
-  return responseArray[Math.floor(Math.random() * responseArray.length)];
+  const answer = responseArray[Math.floor(Math.random() * responseArray.length)];
+  document.getElementById('answer').style.visibility = 'visible';
+  document.getElementById('answerText').innerHTML = answer;
 }
 
-
-// Tests
-console.log(getResponse());
+// Calls the response function when the button is clicked
+document.getElementById('askButton').onclick = function clickAsk() {
+  getResponse();
+};
